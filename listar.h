48b948a314084a561d0592d509fac64708b439f5
Pedro Henrique <pedrohcavalcante>
2016-06-04 02:02:11 -0300

    inicio da função de listagem

--- listar.h
+++ listar.h
@@ -1,3 +1,14 @@
+#ifndef _LISTAR_H_
+#define _LISTAR_L_
+
 #include <iostream>
 #include <getopt.h>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+
 #include "inserir.h"
+
+void printFile();
+
+#endif
