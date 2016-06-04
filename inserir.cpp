7f49358eba43a0efd9b833e28d5b469045cf77c4
Pedro Henrique <pedrohcavalcante>
2016-06-04 00:10:10 -0300

    inserção da função de imprimir

--- inserir.cpp
+++ inserir.cpp
@@ -3,13 +3,13 @@
 #include <fstream>
 #include <sstream>
 #include "inserir.h"
-
+using namespace std;
 void OpenFile(char** fileName){
 	
-	std::cout << *fileName << std::endl;
+	//std::cout << *fileName << std::endl;
 	//char file = fileName;
 	std::ofstream base_busca;
-	base_busca.open("base_busca.txt");
+	base_busca.open("base_busca.txt",std::ofstream::app);
 	if (!base_busca.is_open()){
 		std::cout << "Não foi possível abrir o arquivo" << std::endl;
 		//return 0;
@@ -17,4 +17,20 @@ void OpenFile(char** fileName){
 		std::cout << "rolou" << std::endl;
 	}
 	base_busca << *fileName << "\n";
+	base_busca.close();
+}
+
+void printFile(){
+	std::istringstream iss;
+	std::ifstream base_busca;
+	std::string namefile;
+	base_busca.open("base_busca.txt", std::ofstream::app);
+	if (!base_busca.is_open()){
+		std::cout << "Errado ao abrir" << std::endl;
+	}
+	while(!base_busca.fail()){
+		//std::cout << "entrou" << std::endl;
+		std::getline(base_busca, namefile);
+		std::cout << namefile << std::endl;
+	}
 }
