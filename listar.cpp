053caf7cb956b6ebfb68727fc15854e275d9157d
Pedro Henrique <pedrohcavalcante>
2016-06-04 03:41:16 -0300

    nenhuma alteração funcional detectada

--- listar.cpp
+++ listar.cpp
@@ -3,18 +3,21 @@
 #include <fstream>
 #include <sstream>
 #include "listar.h"
-
+using namespace std;
 
 void printFile(){
+	// ================ DIRETIVAS DE ARQUIVO ================
 	std::istringstream iss;
 	std::ifstream base_busca;
 	std::string namefile;
+	// ================ FIM ================
+	// ================ VARIAVEIS ================
+	// ================ FIM ================
 	base_busca.open("base_busca.txt", std::ofstream::app);
 	if (!base_busca.is_open()){
 		std::cout << "Errado ao abrir" << std::endl;
 	}
 	while(!base_busca.fail()){
-		//std::cout << "entrou" << std::endl;
 		std::getline(base_busca, namefile);
 		std::cout << namefile << std::endl;
 	}
