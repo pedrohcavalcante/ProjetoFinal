ba51a77dec9b49cd0cff4df220afe64752951928
Pedro Henrique <pedrohcavalcante>
2016-06-04 03:42:02 -0300

    inserção de um comando de listagem que estava faltando

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char * argv[]){
 	    {"inserir",        required_argument,        0, 'i'}, 
     	{"remover",        required_argument,        0, 'r'},
     	{"lista_insercao", required_argument,        0, 'l'},
+    	{"li",             no_argument,              0, 'o'},
     	{"la",             no_argument,              0, 'a'}, 
     	{"lt",             no_argument,              0, 't'},
     	{0,0,0,0},
@@ -25,6 +26,7 @@ int main(int argc, char * argv[]){
 	bool iflag = false;
 	bool rflag = false;
 	bool lflag = false;
+	bool liflag = false;
 	bool laflag = false;
 	bool ltflag = false;
 	int index;
@@ -35,7 +37,7 @@ int main(int argc, char * argv[]){
 
 
 	while (iarg != -1){
-		iarg = getopt_long_only(argc, argv, "i:r:l:at", longopts, &index);
+		iarg = getopt_long_only(argc, argv, "i:r:l:oat", longopts, &index);
 		switch(iarg){
 			case 'i':
 				if (iflag){
@@ -56,10 +58,15 @@ int main(int argc, char * argv[]){
 				std::cout << "ENtrou em L" << std::endl;
 				lflag = true;
 				break;
+			case 'o':
+				std::cout << "Entrou em LI" << std::endl;
+				liflag = true;
+				printFile();
+				break;
 			case 'a':
 				std::cout << "Entrou em la" << std::endl;
 				laflag = true;
-				printFile();
+				
 				break;
 			case 't':
 				std::cout << "Entrou em lt" << std::endl;
