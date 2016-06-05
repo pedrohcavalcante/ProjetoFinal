dcc3f2bbc40b55bd5066bfb3b050a9d134d094d0
Pedro Henrique <pedrinho.sjs@ufrn.edu.br>
2016-06-05 05:17:23 -0300

    alteração do main para incluir função la

--- listar.h
+++ listar.h
@@ -9,5 +9,7 @@
 #include "inserir.h"
 
 void printFile();
+void printFileCresc();
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char * argv[]){
 					return 0.;
 				}
 				laflag = true;
-				
+				printFileCresc();
 				break;
 			case 't':
 				std::cout << "Entrou em lt" << std::endl;
