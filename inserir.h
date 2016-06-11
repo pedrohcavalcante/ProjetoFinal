de84a36b7f604c76f5b0d53c7829482ef2244878
Pedro Henrique <pedrinho.sjs@ufrn.edu.br>
2016-06-11 05:43:01 -0300

    funções adicionais

--- inserir.h
+++ inserir.h
@@ -6,10 +6,26 @@
 #include <fstream>
 #include <sstream>
 
+struct INDICE{
+	char Palavra[20];
+	int IndLinha[30];
+	int Controle;
+	INDICE* prox = NULL;
+};
 
 
+struct DOCS {
+	char NameDoc [20];
+	int Palavras;
+	DOCS *prox;
+	INDICE *Ind;
+};
+
+
+DOCS* criarlista();
 void OpenFile(char** fileName);
-
+void OpenFile_noAPP(char** fileName);
+void remover_file(char** fileName);
 
 
 
