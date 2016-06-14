e7b14ff3054af99a8f9319d2bdea47c07079ba58
Pedro Henrique <pedrinho.sjs@ufrn.edu.br>
2016-06-13 22:52:26 -0300

    alteração de listagem por ordem alfabética

--- listar.h
+++ listar.h
@@ -1,33 +1,11 @@
+
 #ifndef _LISTAR_H_
-#define _LISTAR_L_
+#define _LISTAR_H_
+#include "PreProcessing.h"
 
-#include <iostream>
-#include <getopt.h>
-#include <cstdlib>
-#include <fstream>
-#include <sstream>
-#include "inserir.h"
-
-typedef struct{
-	int inicio;
-	int tamanho;
-	int fim;
-	int *elementos;
-}Fila;
-
-Fila* criarFila();
-Fila* criarFila(int tamanho);
-bool enqueue(Fila* fila, int valor);
-bool dequeue(Fila* fila);
-bool filaVazia(Fila* fila);
-bool filaCheia(Fila* fila);
-void movimentar(Fila* fila);
-void imprimir(Fila* fila);
-void testes(Fila* fila);
 
 void printFile();
-void printFileCresc();
-int contaLinha();
-
+void printAlf(DOCS** lista);
+void QuickSort( std::string v[], int left, int right);
 
 #endif
